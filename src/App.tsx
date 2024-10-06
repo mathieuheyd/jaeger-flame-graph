479377bda77ba67dd0a7b32491e6e6de38a488c5
Mathieu Heyd <mathieu.heyd@gmail.com>
2024-10-06 11:05:14 +0200

    Add basic flame chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FlameChartComponent } from 'flame-chart-js/react';
 
 function App() {
+  const flameChartData = [{
+    name: 'foo',
+    start: 300,
+    duration: 200,
+    type: 'task',
+    children: [
+        {
+            name: 'foo',
+            start: 310,
+            duration: 50,
+            type: 'sub-task',
+            color: '#AA0000',
+        },
+    ],
+  }];
+
+  const settings = {
+    hotkeys: {
+      active: true,  // enable navigation using arrow keys
+      scrollSpeed: 0.5, // scroll speed (ArrowLeft, ArrowRight)
+      zoomSpeed: 0.001, // zoom speed (ArrowUp, ArrowDown, -, +)
+      fastMultiplayer: 5, // speed multiplier when zooming and scrolling (activated by Shift key)
+    },
+    options: {
+      tooltip: () => {
+          /*...*/
+      }, // see section "Custom Tooltip" below
+      timeUnits: 'ms',
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FlameChartComponent
+        data={flameChartData}
+        settings={settings}
+        />
     </div>
   );
 }

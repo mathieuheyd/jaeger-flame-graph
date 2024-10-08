958aa2b3eb75cc618977b2d707d8877c8c1c0854
Mathieu Heyd <mathieu.heyd@gmail.com>
2024-10-08 19:30:30 +0200

    unclutter out of bounds spans

--- src/flame-chart/flame-chart-utils.tsx
+++ src/flame-chart/flame-chart-utils.tsx
@@ -1,10 +1,16 @@
 import { FlameChartNode } from "flame-chart-js/.";
 
-function unclutter(flameChart: FlameChartNode) {
-  unclutterRecursively(flameChart);
+type EnrichedFlameChartNode = FlameChartNode & {
+  isPushDownNode?: boolean;
 }
 
-function unclutterRecursively(node: FlameChartNode) {
+function unclutter(flameChart: FlameChartNode) {
+  // unclutter inbounds recursively first to give prioriry to siblings 
+  unclutterInbounds(flameChart);
+  unclutterOutOfBounds(flameChart);
+}
+
+function unclutterInbounds(node: FlameChartNode) {
   if (node.children === undefined || node.children.length === 0)
       return;
 
@@ -17,21 +23,51 @@ function unclutterRecursively(node: FlameChartNode) {
     let child = node.children[i];
     const childEnd = child.start + child.duration;
     
-    unclutterRecursively(child);
+    unclutterInbounds(child);
+
+    if (!isInboundNode(child, node))
+      continue;
 
     const maxDepthForInterval = depthTracker.getMaxDepthForInterval(child.start, childEnd);
     if (maxDepthForInterval > 0) {
-      // we need to unclutter here
-      //child.color = '#AA0000';
       node.children[i] = pushDownNode(child, maxDepthForInterval);
       child = node.children[i];
     }
 
-    const childMaxDepth = nodeMaxDepth(child);
+    const childMaxDepth = nodeInboundMaxDepth(child);
     depthTracker.addIntervalWithDepth(child.start, childEnd, childMaxDepth);
   }
 }
 
+function unclutterOutOfBounds(node: FlameChartNode) {
+  const maxDepthInbounds = getAllMaxDepthsInbound(node);
+  unclutterOutOfBoundsRecursively(node, maxDepthInbounds, 0);
+}
+
+function unclutterOutOfBoundsRecursively(node: FlameChartNode, maxDepths: DepthTracker, nodeDepth: number) {
+  if (node.children === undefined || node.children.length === 0)
+    return;
+
+  for (let i = 0; i < node.children.length; ++i) {
+    let child = node.children[i];
+
+    let childDepth = nodeDepth + 1;
+    if (!isInboundNode(child, node)) {
+      const childEnd = child.start + child.duration;
+      const currentMaxDepth = maxDepths.getMaxDepthForInterval(child.start, childEnd);
+      if (nodeDepth <= currentMaxDepth) {
+        const pushedDownDepth = currentMaxDepth - nodeDepth;
+        const pushedDownNode = pushDownNode(child, pushedDownDepth);
+        node.children[i] = pushedDownNode;
+        childDepth += pushedDownDepth;
+      }
+      const nodeMaxDepth = nodeInboundMaxDepth(child);
+      maxDepths.addIntervalWithDepth(child.start, childEnd, nodeMaxDepth + nodeDepth);
+    }
+
+    unclutterOutOfBoundsRecursively(child, maxDepths, childDepth);
+  }
+}
 type Depth = {
   start: number;
   end: number;
@@ -57,7 +93,7 @@ class DepthTracker {
   }
 }
 
-function pushDownNode(node: FlameChartNode, depth: number): FlameChartNode {
+function pushDownNode(node: FlameChartNode, depth: number): EnrichedFlameChartNode {
   if (depth <= 0)
     return node;
 
@@ -65,7 +101,8 @@ function pushDownNode(node: FlameChartNode, depth: number): FlameChartNode {
     name: "",
     start: node.start,
     duration: 0,
-    children: [node]
+    children: [node],
+    isPushDownNode: true
   };
 
   return pushDownNode(pushedDown, depth - 1);
@@ -79,18 +116,50 @@ function compareNodes(nodeA: FlameChartNode, nodeB: FlameChartNode) {
     return nodeB.duration - nodeA.duration;
 }
 
-function nodeMaxDepth(node: FlameChartNode): number {
+function nodeInboundMaxDepth(node: FlameChartNode): number {
+  return nodeMaxDepthInternal(node, true);
+}
+
+function nodeMaxDepthInternal(node: FlameChartNode, onlyInbound: boolean): number {
   if (node.children === undefined || node.children.length === 0)
     return 1;
 
   let childrenMaxDepth = 0;
   node.children.forEach(child => {
-    const childMaxDepth = nodeMaxDepth(child);
+    if (onlyInbound && !isInboundNode(child, node))
+      return;
+    const childMaxDepth = nodeMaxDepthInternal(child, onlyInbound);
     if (childMaxDepth > childrenMaxDepth)
         childrenMaxDepth = childMaxDepth;
   });
 
   return 1 + childrenMaxDepth;
+}
+
+function getAllMaxDepthsInbound(node: FlameChartNode): DepthTracker {
+  const depthTracker = new DepthTracker();
+  fillAllMaxDepthsInbound(node, 1, depthTracker);
+  return depthTracker;
+}
+
+function fillAllMaxDepthsInbound(node: FlameChartNode, nodeDepth: number, depthTracker: DepthTracker) {
+  depthTracker.addIntervalWithDepth(node.start, node.start + node.duration, nodeDepth);
+
+  if (node.children === undefined || node.children.length === 0)
+    return;
+
+  node.children.forEach(child => {
+    if (isInboundNode(child, node)) {
+      fillAllMaxDepthsInbound(child, nodeDepth + 1, depthTracker);
+    }
+  });
+}
+
+function isInboundNode(node: EnrichedFlameChartNode, parent: FlameChartNode): boolean {
+  // Exception for pushed down nodes
+  if (node.isPushDownNode === true)
+    return isInboundNode(node.children![0], parent);
+  return (node.start + node.duration <= parent.start + parent.duration);
 }
 
 export { unclutter };

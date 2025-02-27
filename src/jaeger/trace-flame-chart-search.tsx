5c2c1a3b5c8e04a3c285411fe96ef15b11c80e3b
Mathieu Heyd <mathieu.heyd@gmail.com>
2025-02-27 23:04:30 +0100

    Add method to search nodes in graph

---
+++ src/jaeger/trace-flame-chart-search.tsx
@@ -0,0 +1,28 @@
+import { FlameChartNode } from "flame-chart-js/.";
+import { EnrichedFlameChartNode } from "./trace-flame-chart";
+import { Span } from "./trace";
+
+function search(rootNodes: EnrichedFlameChartNode[], text: string): FlameChartNode[] {
+  const matchingSpans: FlameChartNode[] = [];
+
+  rootNodes.forEach(rootNode => searchInternal(rootNode, text, matchingSpans));
+
+  return matchingSpans;
+}
+
+function searchInternal(node: EnrichedFlameChartNode, text: string, matchingSpans: FlameChartNode[]) {
+  if (node.sourceSpan !== undefined && doesSpanMatch(node.sourceSpan, text))
+    matchingSpans.push(node);
+
+  node.children?.forEach(child => searchInternal(child as EnrichedFlameChartNode, text, matchingSpans));
+}
+
+function doesSpanMatch(span: Span, text: string): boolean {
+  return span.operationName.includes(text) ||
+    span.tags.some(tag => tag.key.includes(text) || tag.value.toString().includes(text)) ||
+    span.logs.some(log => log.fields.some(logField => logField.key.includes(text) || logField.value.toString().includes(text)));
+}
+
+export {
+  search
+}
